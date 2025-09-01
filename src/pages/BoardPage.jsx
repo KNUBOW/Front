ea7266b7f52ff70f89e90c6dab2fba30eeaa3966
KimSnowFlower <kimbona1148@gmail.com>
2025-09-01 15:02:11 +0900

    feat: #28 add Board Page css

--- src/pages/BoardPage.jsx
+++ src/pages/BoardPage.jsx
@@ -1,8 +1,29 @@
+import TopBar from '../components/TopBar';
+import TopNav from '../components/TopNav';
+import TabBar from '../components/TabBar';
+import '../styles/TopShell.css';
+import '../styles/BoardPage.css';
+
 const BoardPage = () => {
+
+    const navItems = [
+        { label: "오늘 뭐 해먹지?", to: "/" },
+        { label: "추천 요리", to: "/recommend" },
+        { label: "게시판", to: "/board" },
+        { label: "랭킹", to: "/rank" },
+    ];
+
     return (
-        <div>
-            <h1>게시판 페이지</h1>
-            <p>게시판 콘텐츠가 여기에 표시됩니다.</p>
+        <div className="board-page">
+            <div className="board-wrap layout-grid">
+                <TopBar />
+                <TopNav items={navItems} />
+
+                <div>
+                    <h1>게시판 페이지</h1>
+                </div>
+                <TabBar />
+            </div>
         </div>
     );
 }
